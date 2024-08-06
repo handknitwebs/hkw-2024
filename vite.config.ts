92a79552503894b1d4589cfa610bde89f391a66b
hkwadam <87677738+hkwadam@users.noreply.github.com>
2024-08-06 14:03:19 -0700

    removed unnecessary comment.

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  // remove base and run build if pushing to bluehost -- grab files from dist 
+export default defineConfig({ 
   base: "./",
   plugins: [react()],
 })
