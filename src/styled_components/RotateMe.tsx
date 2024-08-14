019c8fed919f63ea2199c9036550e82f92fb280b
Allie Soldau <alliesoldau@gmail.com>
2024-08-14 13:25:55 -0400

    slowed down lets talk slide and rotation

--- src/styled_components/RotateMe.tsx
+++ src/styled_components/RotateMe.tsx
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 
 const RotateMe = styled.div`
     @media (min-width: 1081px) {
-        transition: transform 0.4s ease;
+        transition: transform 0.6s ease;
         transform: none;
     }
 `;
--- src/styled_components/SlideOnHover.tsx
+++ src/styled_components/SlideOnHover.tsx
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 
 const SlideOnHover = styled.div`
   @media (min-width: 1081px) {
-    transition: transform 0.4s ease;
+    transition: transform 0.6s ease;
     transform: translateY(0) rotate(0);
     &:hover {
       transform: translateY(-3rem);
