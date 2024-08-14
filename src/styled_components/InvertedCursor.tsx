6b5d1dfb2f609276f44a487959c7f00b7f603d24
Allie Soldau <alliesoldau@gmail.com>
2024-08-14 13:45:18 -0400

    made it so click events still work

--- src/styled_components/InvertedCursor.tsx
+++ src/styled_components/InvertedCursor.tsx
@@ -1,19 +1,21 @@
 /** @jsxImportSource @emotion/react */
 import styled from '@emotion/styled';
-
+// https://dev.to/mattmarquise/how-to-create-a-custom-circular-cursor-for-your-website-4i7p
 const InvertedCursor = styled.div`
-    position: absolute;
-    width: 40px;
-    height: 40px;
-    background: #fff;
-    border-radius: 50%;
-    top: var(--y, 0);
-    left: var(--x, 0);
-    transform: translate(-50%, -50%);
-    z-index: 999;
-    mix-blend-mode: difference;
-    transition: transform .2s;
-    pointer-events: none;
+    @media (min-width: 1081px) {
+        position: absolute;
+        width: 1.5rem;
+        height: 1.5rem;
+        background: #fff;
+        border-radius: 50%;
+        top: var(--y, 0);
+        left: var(--x, 0);
+        transform: translate(-50%, -50%);
+        z-index: 999;
+        mix-blend-mode: difference;
+        transition: transform .2s;
+        pointer-events: none;
+    }
 `;
 
 export default InvertedCursor;
