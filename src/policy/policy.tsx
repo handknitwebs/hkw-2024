bea6f23a7fa26f048fba721db5f81a0f6ba013c7
Adam Townsend <adam@desnudopenguino.com>
2024-12-19 13:14:53 -0800

    moved files around for adding a 2nd page, and added 2nd page for fulfillment policy for Stripe

---
+++ src/policy/policy.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDom.render(
+  <React.StrictMode>
+    <h1>Heading!</h1>
+    <p>here is some content, please work!</p>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // remove base and run build if pushing to bluehost -- grab files from dist 
+  root,
   base: "/hkw-2024/",
   plugins: [react()],
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        policy: resolve(root, 'policy', 'index.html'),
+      }
+    }
+  }
 })
